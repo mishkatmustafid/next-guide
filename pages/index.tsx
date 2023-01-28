4e00cbda35f7ccbc742054e841aee2d72308e50f
Mishkat Mustafid <mishkatmustafid@gmail.com>
2023-01-28 12:28:44 +0600

    HomePage updated

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,12 @@
 import Head from 'next/head'
 import type { GetStaticProps, NextPage } from 'next'
 import styles from '@/styles/Home.module.css'
+import { Character, GetCharacterResults } from '@/types'
+import Image from 'next/image'
+import imageLoader from '@/imageLoader'
+import Link from 'next/link'
 
-export default function Home() {
+const Home: NextPage<{ characters: Character[] }> = ({ characters }) => {
   return (
     <div className={styles.container}>
       <Head>
@@ -11,6 +15,38 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      {/* DB_CONNECT: {process.env.NEXT_PUBLIC_DB_CONNECT} */}
+      {characters.map((character) => {
+        return (
+          <div key={character.id}>
+            <Link href={`/characters/${character.id}`}>
+              <h3>{character.name}</h3>
+            </Link>
+
+            <Image
+              loader={imageLoader}
+              unoptimized
+              src={character.image}
+              alt={character.name}
+              width="200"
+              height="200"
+            />
+          </div>
+        )
+      })}
     </div>
   )
 }
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const res = await fetch('https://rickandmortyapi.com/api/character')
+  const { results }: GetCharacterResults = await res.json()
+
+  return {
+    props: {
+      characters: results,
+    },
+  }
+}
+
+export default Home
